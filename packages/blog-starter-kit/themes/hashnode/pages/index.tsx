324f27edf757e8bbca27b4d3c29ffc0c5a66a718
Harendra Kumar Kanojiya <harendraverma21@gmail.com>
2024-07-30 15:24:37 +0530

    issue

--- packages/blog-starter-kit/themes/hashnode/pages/index.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/index.tsx
@@ -133,21 +133,24 @@ export default function Index(
 					/>
 
 					<script
-						type="application/ld+json"
+						type="text/javascript"
 						dangerouslySetInnerHTML={{
-							var _paq = window._paq = window._paq || [];
-							/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-							_paq.push(['trackPageView']);
-							_paq.push(['enableLinkTracking']);
-							(function () {
-								var u = "https://analytics.with-code-examples.online/";
-								_paq.push(['setTrackerUrl', u + 'matomo.php']);
-								_paq.push(['setSiteId', '7']);
-								var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-								g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
-							})();
+							__html: `
+								var _paq = window._paq = window._paq || [];
+								/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+								_paq.push(['trackPageView']);
+								_paq.push(['enableLinkTracking']);
+								(function() {
+									var u = "https://analytics.with-code-examples.online/";
+									_paq.push(['setTrackerUrl', u + 'matomo.php']);
+									_paq.push(['setSiteId', '7']);
+									var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+									g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+								})();
+								`,
 						}}
 					/>
+
 
 
 				</Head>
