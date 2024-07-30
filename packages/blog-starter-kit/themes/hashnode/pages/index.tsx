c3bf505c679ea53521ef95a7c18d6ad1bae42c90
Harendra Kumar Kanojiya <harendraverma21@gmail.com>
2024-07-30 15:32:52 +0530

    analytics

--- packages/blog-starter-kit/themes/hashnode/pages/index.tsx
+++ packages/blog-starter-kit/themes/hashnode/pages/index.tsx
@@ -136,10 +136,7 @@ export default function Index(
 						type="text/javascript"
 						dangerouslySetInnerHTML={{
 							__html: `
-								<!-- Matomo -->
-<script>
   var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
@@ -148,11 +145,7 @@ export default function Index(
     _paq.push(['setSiteId', '7']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<!-- End Matomo Code -->
-
-								`,
+  })();`,
 						}}
 					/>
 
